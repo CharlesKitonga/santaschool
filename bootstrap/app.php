17288bdf81c68a45911811af5ca004b158cf471b
CharlesKitonga <charleskyalo006@gmail.com>
2026-09-18 10:29:12 +0300

    Adding vercel trusted proxies in laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
 
-        $middleware->trustedProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR |
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR |
             Request::HEADER_X_FORWARDED_HOST |
             Request::HEADER_X_FORWARDED_PORT |
             Request::HEADER_X_FORWARDED_PROTO |
